af4017c0edcac5125496b9f53d9ea07e8ddd4296
abouchaud2023 <art.bouchaud@gmail.com>
2025-03-18 11:10:32 +0100

    ajout fonctionnalité affichage password barre

--- src/main/resources/static/js/home/register.js
+++ src/main/resources/static/js/home/register.js
@@ -1,4 +1,85 @@
 $(document).ready(function () {
+
+    // Afficher/masquer le bloc des critères lors du focus/blur
+    $('#passwordReg').on('focus', function () {
+        $('#passwordMessage').show();
+    }).on('blur', function () {
+        $('#passwordMessage').hide();
+    });
+
+    // Écoute de l'input sur le mot de passe
+    $('#passwordReg').on('keyup', function () {
+        var password = $(this).val();
+        var score = calculateStrength(password);
+        updateProgressBar(score);
+        updateCriteria(password);
+    });
+
+    // Fonction de calcul de la force du mot de passe (score de 0 à 4)
+    function calculateStrength(password) {
+        var score = 0;
+        if (/[a-z]/.test(password)) {
+            score++;
+        }
+        if (/[A-Z]/.test(password)) {
+            score++;
+        }
+        if (/[0-9]/.test(password)) {
+            score++;
+        }
+        if (password.length >= 8) {
+            score++;
+        }
+        return score;
+    }
+
+    // Mise à jour de la progressbar Bootstrap selon le score
+    function updateProgressBar(score) {
+        var percentage = (score / 4) * 100;
+        var progressBar = $('#passwordProgress');
+        progressBar.css('width', percentage + '%');
+        progressBar.attr('aria-valuenow', percentage);
+
+        // Définition de la couleur en fonction du score
+        if (score <= 1) {
+            progressBar.removeClass().addClass('progress-bar bg-danger');
+        } else if (score === 2) {
+            progressBar.removeClass().addClass('progress-bar bg-warning');
+        } else if (score >= 3) {
+            progressBar.removeClass().addClass('progress-bar bg-success');
+        }
+    }
+
+    // Met à jour les messages de critères en fonction du contenu du mot de passe
+    function updateCriteria(password) {
+        // Critère : lettre minuscule
+        if (/[a-z]/.test(password)) {
+            $('#letter').removeClass('text-danger').addClass('text-success').html('<i class="fa-solid fa-check"></i> Une lettre <b>minuscule</b>');
+        } else {
+            $('#letter').removeClass('text-success').addClass('text-danger').html('<i class="fa-solid fa-xmark"></i> Une lettre <b>minuscule</b>');
+        }
+        // Critère : lettre majuscule
+        if (/[A-Z]/.test(password)) {
+            $('#capital').removeClass('text-danger').addClass('text-success').html('<i class="fa-solid fa-check"></i> Une lettre <b>majuscule</b>');
+        } else {
+            $('#capital').removeClass('text-success').addClass('text-danger').html('<i class="fa-solid fa-xmark"></i> Une lettre <b>majuscule</b>');
+        }
+        // Critère : nombre
+        if (/[0-9]/.test(password)) {
+            $('#number').removeClass('text-danger').addClass('text-success').html('<i class="fa-solid fa-check"></i> Un <b>chiffre</b>');
+        } else {
+            $('#number').removeClass('text-success').addClass('text-danger').html('<i class="fa-solid fa-xmark"></i> Un <b>chiffre</b>');
+        }
+        // Critère : longueur minimale
+        if (password.length >= 8) {
+            $('#length').removeClass('text-danger').addClass('text-success').html('<i class="fa-solid fa-check"></i> Au moins <b>8 caractères</b>');
+        } else {
+            $('#length').removeClass('text-success').addClass('text-danger').html('<i class="fa-solid fa-xmark"></i> Au moins <b>8 caractères</b>');
+        }
+    }
+
+
+    // Votre code existant pour la soumission du formulaire
     $('#registerForm').on('submit', function (e) {
         e.preventDefault();
         $('#registerSpinner').removeClass('d-none');
